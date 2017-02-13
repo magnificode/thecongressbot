7e332e2c55260246800cccff6b4274bc2ae4ef2e
Dominic Magnifico <dominicmagnifico@gmail.com>
2017-02-12 21:25:01 -0700

    Add additional logs to provide insight to what's happening, tweak environment variables to prevent login issues. Remove queued tweets from array once they're tweeted and ensure their ID is added to the tweeted array.

--- bot.js
+++ bot.js
@@ -7,7 +7,8 @@
 require('dotenv').config();
 var Twit = require('twit');
 var length = 78;
-var tweeted =  [348982,348992,348977,348976,348975,348969,348971,348980,348985,348986,348981,348973,348972,348978,348988,348991,348987,348979,348984,348970,348968,348989,348983,348990,348974,348965,348966,348963,348964,348962,348967,348899,348869,348860,348833,348864,348841,348917,348901,348843,348928,348912,348855,348908,348957,348834,348829,348903,348947,348878,348904,348830,348880,348858,348914,348936,348863,348955,348886,348900,348902,348944,348854,348959,348938,348882,348951,348856,348890,348952,348835,348960,348949,348881,348839,348935,348950,348884,348916,348897,348896,348927,348845,348877,348937,348907,348954,348892,348925,348923,348932,348883,348838,348911,348893,348872,348946,348889,348836,348870];
+var tweeted =  [348982,348992,348977,348976,348975,348969,348971,348980,348985,348986,348981,348973,348972,348978,348988,348991,348987,348979,348984,348970,348968,348989,348983,348990,348974,348965,348966,348963,348964,348962,348967,348899,348869,348860,348833,348864,348841,348917,348901,348843,348928,348912,348855,348908,348957,348834,348829,348903,348947,348878,348904,348830,348880,348858,348914,348936,348863,348955,348886,348900,348902,348944,348854,348959,348938,348882,348951,348856,348890,348952,348835,348960,348949,348881,348839,348935,348950,348884,348916,348897,348896,348927,348845,348877,348937,348907,348954,348892,348925,348923,348932,348883,348838,348911,348893,348872,348946,348889,348836,348870,348993,348994];
+var runCount = 0;
 
 var Bot = new Twit({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -24,8 +25,7 @@ function BotInit() {
 }
 
 function BotTweet() {
-  var request = require("request");
-  let runCount = 0;
+  var request = require('request');
   var url = "https://www.govtrack.us/api/v2/bill?congress=115&order_by=-introduced_date";
 
   request({
@@ -50,10 +50,16 @@ function BotTweet() {
           const bill = bills[id];
           const link = bill.link;
           const title = bill.title;
+          const billId = bill.id;
           const truncTitle = title.substring(0, length) + "...";
           const tweet = "Congress just introduced '" + truncTitle + "' " + link;
 
+          console.log('Bill ID: ' + billId + ' added to the builtTweets array.');
+          console.log('**********************');
           builtTweets.push(tweet);
+          console.log('Bill ID: ' + billId + ' added to the tweeted array.');
+          console.log('**********************');
+          tweeted.push(billId);
         }
       }
 
@@ -71,10 +77,20 @@ function BotTweet() {
       };
 
       function staggerTweet() {
-        builtTweets.delayedForEach(function(tweet){
-          Bot.post('statuses/update', { status: tweet }, function(err, data, response) {
-            console.log(tweet)
+        builtTweets.delayedForEach(function(tweet, index, array){
+          //Report number of tweets in the pipeline.
+          console.log( 'There are ' + builtTweets.length + ' tweets queued.' );
+          console.log('**********************');
+          Bot.post('statuses/update', { status: array[0] }, function(err, data, response) {
+            if( array[0] ) {
+              console.log('Tweeted: ' + array[0]);
+              console.log('**********************');
+            }
+            if( err ) {
+              console.log(err);
+            }
           });
+          builtTweets.splice( 0,1 );
         }, 15*60*1000); //15 minutes
       }
 
@@ -85,11 +101,13 @@ function BotTweet() {
     }
   });
 
-  //Check the JSON file ever 30 minutes.
-  setInterval(BotTweet, 30*60*1000);
-
-  console.log('Request sent. Iteration;' + runCount);
+  console.log('Request sent. Iteration: ' + runCount);
+  console.log('**********************');
   runCount++;
+
+  //Check the JSON file ever 30 minutes.
+  setInterval(BotTweet, 10*1000);
+
 }
 
 /* Initiate the Bot */
