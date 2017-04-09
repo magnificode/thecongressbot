c67adc93953359dcd56e0610326ee2497243001e
Dominic Magnifico <dominicmagnifico@gmail.com>
2017-04-09 17:20:40 -0600

    Add firebase to actually maintain a database of tweeted bill ID's.

--- bot.js
+++ bot.js
@@ -5,9 +5,26 @@
 */
 
 require('dotenv').config();
+
+
+var firebase = require("firebase");
+
+var config = {
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASEURL,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID
+};
+firebase.initializeApp(config);
+
+const db = firebase.database();
+const ref = db.ref('/');
+
 var Twit = require('twit');
 var length = 78;
-var tweeted =  [348982,348992,348977,348976,348975,348969,348971,348980,348985,348986,348981,348973,348972,348978,348988,348991,348987,348979,348984,348970,348968,348989,348983,348990,348974,348965,348966,348963,348964,348962,348967,348899,348869,348860,348833,348864,348841,348917,348901,348843,348928,348912,348855,348908,348957,348834,348829,348903,348947,348878,348904,348830,348880,348858,348914,348936,348863,348955,348886,348900,348902,348944,348854,348959,348938,348882,348951,348856,348890,348952,348835,348960,348949,348881,348839,348935,348950,348884,348916,348897,348896,348927,348845,348877,348937,348907,348954,348892,348925,348923,348932,348883,348838,348911,348893,348872,348946,348889,348836,348870,348993,348994];
+var tweeted = [];
 var runCount = 0;
 
 var Bot = new Twit({
@@ -48,28 +65,36 @@ function BotTweet() {
         pos++;
       } );
 
-      console.log(idsToTweet);
+      ref.child('tweeted').once('value', (snap) => {
+        // For each of the ids in idsToTweet, check to see if the ID has already been tweeted.
+        for( let id in idsToTweet ) {
+          if( snap.val().includes(idsToTweet[id]) ){
+            console.log(idsToTweet[id] + ' has already been tweeted');
+          } else {
+            // Set some variables for the current bill.
+            const bill = bills[id];
+            const link = bill.link;
+            const title = bill.title;
+            const billId = bill.id;
+            const truncTitle = title.substring(0, length) + "...";
+            const tweet = "Congress just introduced '" + truncTitle + "' " + link;
+          
+            console.log('Bill ID: ' + billId + ' added to the builtTweets array.');
+            console.log('**********************');
+            builtTweets.push(tweet);
+            console.log('Bill ID: ' + billId + ' added to the tweeted array.');
+            console.log('**********************');
 
-      // For each of the ids in idsToTweet, check to see if the ID has already been tweeted.
-      for( let id in idsToTweet ) {
-        // If that particular ID hasn't been tweeted, proceed.
-        if( !tweeted.includes(idsToTweet[id]) ) {
-          // Set some variables for the current bill.
-          const bill = bills[id];
-          const link = bill.link;
-          const title = bill.title;
-          const billId = bill.id;
-          const truncTitle = title.substring(0, length) + "...";
-          const tweet = "Congress just introduced '" + truncTitle + "' " + link;
+            const snapVal = snap.val()
+            snapVal.push(billId);
+            console.log(snapVal);
 
-          console.log('Bill ID: ' + billId + ' added to the builtTweets array.');
-          console.log('**********************');
-          builtTweets.push(tweet);
-          console.log('Bill ID: ' + billId + ' added to the tweeted array.');
-          console.log('**********************');
-          tweeted.push(billId);
+            ref.set({
+              tweeted: snapVal
+            });
+          }
         }
-      }
+      });
 
       // Delayed foreach function to not innundate twitter with tweets. No more 100 tweet dumps.
       Array.prototype.delayedForEach = function(callback, timeout, thisArg){
